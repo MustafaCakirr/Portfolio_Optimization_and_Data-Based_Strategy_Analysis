bb403919f69169fa5a519ad5c1b10225a7ea8edd
MustafaCakirr <mustafacakir99@hotmail.com>
2025-01-03 23:56:52 +0300

    Initial commit

---
+++ File_Operations_and_Preprocessing.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np 
+
+asel_data   = pd.read_excel("D:/bist_hisseler/aselsan_hisse.xlsx")
+ak_data     = pd.read_excel("D:/bist_hisseler/akbankhisse.xlsx")
+ford_data   = pd.read_excel("D:/bist_hisseler/fordhisse.xlsx")
+gar_data    = pd.read_excel("D:/bist_hisseler/garantihisse.xlsx")
+koc_data    = pd.read_excel("D:/bist_hisseler/kochisse.xlsx")
+thy_data    = pd.read_excel("D:/bist_hisseler/thyhisse.xlsx")
+tupr_data   = pd.read_excel("D:/bist_hisseler/tuprashisse.xlsx")
+
+asel_data   = asel_data.drop(['Sermaye(mn TL)'],axis = 1)
+ak_data     = ak_data.drop(['Sermaye(mn TL)'],axis = 1)
+ford_data   = ford_data.drop(['Sermaye(mn TL)'],axis = 1)
+gar_data    = gar_data.drop(['Sermaye(mn TL)'],axis = 1)
+koc_data    = koc_data.drop(['Sermaye(mn TL)'],axis = 1)
+thy_data    = thy_data.drop(['Sermaye(mn TL)'],axis = 1)
+tupr_data   = tupr_data.drop(['Sermaye(mn TL)'],axis = 1)
+
+
+def firm_info(firm):
+    print(firm.to_string())
+
+def asel_info():
+    print(asel_data.to_string())
+
+def ak_info():
+    print(ak_data.to_string()) 
+
+def ford_info():
+    print(ford_data.to_string())
+
+def koc_info():
+    print(koc_data.to_string())
+
+def thy_info():
+    print(thy_data.to_string())
+
+def tupr_info():
+    print(tupr_data.to_string())
+
+def gar_info():
+    print(gar_data.to_string())
+
---
+++ main_portfolio_optimization_and_data-based_strategy_analysis.py
@@ -0,0 +1,18 @@
+﻿import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from data_analysis_and_strategies import *
+from file_operations_and_preprocessing import *
+
+# graph_data(asel_data)
+# firm_info(ak_data)
+# asel_info()
+# graph_data(asel_data)
+# comp_data(asel_data,koc_data,ak_data,ford_data,thy_data)
+# print(cov_matrix())
+# cov_matrix()
+# momentum(ak_data,500)
+# mov_avg(ak_data,50,120)
+
+
